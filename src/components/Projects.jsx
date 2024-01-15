77fff5e08e06342fe5a5862bbee323bd853e2451
Pedro Henrique <104742078+phbrg@users.noreply.github.com>
2024-01-14 21:38:43 -0300

    update an url

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
       <Card title = 'wordle-project' description = 'Wordle is a word guessing game where users guess the word by its letter. BTW, its my first project using React.js' source = 'https://github.com/phbrg/wordle-react' deploy = 'https://ph-wordle.vercel.app/' skills = {[{icon: <i class="fa-brands fa-js"></i>, name: 'Javascript'}, {icon: <i class="fa-brands fa-react"></i>, name: 'React.js'}]} />
       <Card title = 'arkadius' description = 'Arkadius is a social network for users to create and customize profiles as they wish, with the ability to modify content and add links' source = 'https://github.com/phbrg/arkadius' skills = {[{icon: <i class="fa-brands fa-js"></i>, name: 'Javascript'}, {icon: <i class="fa-brands fa-node-js"></i>, name: 'Node.js'}, {icon: <i class="fa-solid fa-database"></i>, name: 'PostgreSQL'}]} />
       <Card title = 'cashflow' description = 'Cashflow is a financial management app where users manage their money and receive investment advice. It was part of a school competition.' source = 'https://github.com/phbrg/wordle-react' skills = {[{icon: <i class="fa-brands fa-js"></i>, name: 'Javascript'}, {icon: <i class="fa-brands fa-node-js"></i>, name: 'Node.js'}, {icon: <i class="fa-solid fa-database"></i>, name: 'PostgreSQL'}]} />
-      <h2 className='subtitle'>You can find many more of my projects  <a href="https://github.com/phbrg" target="_blank">here</a></h2>
+      <h2 className='subtitle'>You can find many more of my projects  <a href="https://github.com/phbrg?tab=repositories" target="_blank">here</a></h2>
     </div>
   )
 }
