92b52cc56510cd4df7e8f06f79959998b947f1fe
Pedro Henrique <104742078+phbrg@users.noreply.github.com>
2024-01-20 18:08:30 -0300

    Update AboutMe.jsx

--- src/components/aboutme/AboutMe.jsx
+++ src/components/aboutme/AboutMe.jsx
@@ -9,7 +9,7 @@ const AboutMe = () => {
         <li>console.log('Hello world!');</li>
         <p>I am a <span>full-stack developer</span> with proficiency in both <span>backend</span> and <span>frontend</span> development. While I am adept at working in both areas, my preference leans towards <span>backend development</span>. Currently, I am broadening my horizons and stepping out of my comfort zone by delving into new technologies.</p>
       </div>
-      <List title={'Knowledge'} elements={['Node.js', 'React.js', 'Jest', 'SQL', 'PostgreSQL', 'MySQL']}/>
+      <List title={'Knowledge'} elements={['Node.js', 'React.js', 'SQL', 'PostgreSQL', 'MySQL']}/>
       <List title={'Links'} elements={[
       <p>Github: <a href='https://github.com/phbrg' target='_blank'>@phbrg</a></p>, 
       <p>Linkedin: <a href='https://www.linkedin.com/in/pedro-henrique-b-bergamin/' target='_blank'>@Pedro Henrique</a></p>, 
