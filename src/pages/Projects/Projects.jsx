2a55902fed6e804fc259293f69d50b1d159e276d
phbrg <xomaicrocefalia123@gmail.com>
2024-02-22 15:08:12 -0300

    fixed a description

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -10,7 +10,7 @@ export const Projects = () => {
       <div className="card-container">
         <Card title='Market Manager' date='Jan, 2024 - Present' description='📊 Node.js and React.js Business Manager with an Interactive Dashboard with Graphs and Management Systems.' tags={['Node.js', 'React.js', 'PostgreSQL']} link='https://github.com/phbrg/market-manager' blank={true}/>
         <Card title='Restaurant Orders' date='Nov, 2023 - Present' description='🍔 Node.js and Angular Restaurant Food Order Web App with an Interactive Menu and Dashboard.' tags={['Node.js', 'Angular', 'PostgreSQL']} link='https://github.com/phbrg/restaurant-order' blank={true}/>
-        <Card title='Wordle' date='Dec, 2023 - Dec, 2023' description='🧶 Wordle: A React.js game where users have to guess the word by the letter.' tags={['React.js']} link='https://github.com/phbrg/wordle-react' blank={true}/>
+        <Card title='Wordle' date='Dec, 2023 - Dec, 2023' description='🧶 A React.js game where users have to guess the word by the letter.' tags={['React.js']} link='https://github.com/phbrg/wordle-react' blank={true}/>
         <Card title='Arkadius' date='Oct, 2023 - Oct, 2023' description='📸Node.js Social Media Platform with Personal Profile, Profile Customization and Share Link on Profile.' tags={['Node.js', 'Handlebars', 'PostgreSQL']} link='https://github.com/phbrg/arkadius' blank={true}/>
         <Card title='Cashflow' date='Nov, 2023 - Nov, 2023' description='💰 Node.js Financial Management App to Manage Your Money and Receive Investment Advice.' tags={['Node.js', 'Handlebars', 'PostgreSQL']} link='https://github.com/phbrg/cashflow' blank={true}/>
       </div>
