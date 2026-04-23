f14c2fc4e623c5aba154a65431423a0082563d1c
Pedro Henrique <104742078+phbrg@users.noreply.github.com>
2026-04-23 19:03:50 -0300

    Delete src directory

--- src/components/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import s from './Footer.module.css'
-
-export const Footer = () => {
-  return (
-    <footer className={s.Footer}>
-      <h1>Made with 💙 by phbrg &copy; 2024</h1>
-    </footer>
-  )
-}
