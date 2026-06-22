b593ab1b5c309193fa5d4cc0f7066ed92b53ed3d
phbrg <pedrohenriquebatistabergamin@gmail.com>
2026-06-22 16:01:52 -0300

    fix: unused declared variable

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Toaster } from "sonner"
 
 import { Header } from "./components/Header/Header";
-import { Footer } from "./components/Footer/Footer";
+// import { Footer } from "./components/Footer/Footer";
 
 import { Home } from "./pages/Home/Home";
 import { About } from "./pages/About/About";
